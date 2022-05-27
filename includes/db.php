7763e2a21a34b562f073fac280734bfb33740596
Jen <maxcruz4455@gmail.com>
2022-05-27 14:20:05 +0800

    fixed changes

--- includes/db.php
+++ includes/db.php
@@ -1,34 +1,10 @@
 <?php
 
-// Server: sql6.freemysqlhosting.net
-// Name: sql6495374
-// Username: sql6495374
-// Password: qNNZ6vhVvn
-// Port number: 3306
-
-$host = "sql6.freemysqlhosting.net";
-$user = "sql6495374";
-$pass = "qNNZ6vhVvn";
-$database = "sql6495374";
+require_once "config.php";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = mysqli_connect($host, $user, $pass, $database);
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB);
 
 if (!$conn) {
     die("Connection unsuccessful " . mysqli_error());
 }
-
-/**
- * application portal table
- * 
- * id int pkey auto_increment not null
- * name varchar(255) not null default ""
- * status varchar(255)not null default 
- * email address(255)
- * contact_no varchar(16)
- * school varchar(255)
- * branch varchar(255)
- * course varchar(255)
- * grdrive_link varcher(255)
- * skills varchar(255)
- */
